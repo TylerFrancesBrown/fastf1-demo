4c13c95c03d292529e9e53642d02f5f3ca1628a4
Tyler Brown <brown.tylerfrances@gmail.com>
2025-08-08 13:53:08 -0400

    Changed for Easier Readability

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import fastf1
 # Enable local cache so data is stored in ./cache (speeds up subsequent runs)
 fastf1.Cache.enable_cache('cache')
 
-# Example: load qualifying session for 2024 Bahrain
-session = fastf1.get_session(2024, 'Bahrain', 'Q')
+YEAR = 2025
+WEEKEND = 'Monaco'
+SESSION = 'Q'
+
+# Example: load qualifying session for YEAR LOCATION SESSION
+session = fastf1.get_session(YEAR, WEEKEND, SESSION)
 session.load()
 
 fastest_lap = session.laps.pick_fastest()
