766df94d98bf4c32b1f354fb54f7a0096355a4c9
Tyler Brown <brown.tylerfrances@gmail.com>
2025-08-12 00:58:17 -0400

    First Function Created.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import fastf1
+import pandas as pd
 
 # Enable local cache so data is stored in ./cache (speeds up subsequent runs)
 fastf1.Cache.enable_cache('cache')
 
 YEAR = 2025
 WEEKEND = 'Montreal'
-SESSION = 'R'
+SESSION = 'Q'
 
 # Example: load qualifying session for YEAR LOCATION SESSION
 session = fastf1.get_session(YEAR, WEEKEND, SESSION)
 session.load()
 
-fastest_lap = session.laps.pick_fastest()
-driver = fastest_lap['Driver']
-lap_time = fastest_lap['LapTime']
+def get_all_fastest_laps(session):
+    fastest_laps = []
 
-print(f"Fastest lap: {driver} - {lap_time}")
+    for driver_id in session.drivers:
+        lap = session.laps.pick_drivers(driver_id).pick_fastest()
+
+        # Ensure we always store as a DataFrame
+        lap_df = pd.DataFrame([lap])
+        fastest_laps.append(lap_df)
+
+    fastest_laps_df = pd.concat(fastest_laps, ignore_index=True)
+    return fastest_laps_df
+
+session_fastest_laps = get_all_fastest_laps(session)
+
+# Sort by lap time and get top 5
+top5 = session_fastest_laps.sort_values("LapTime").head(5)
+
+print(top5[["Driver", "LapTime", "Team"]])
